504c6c73bd57dcfb9d6d051b181299c18753f495
Brian Chang <brimatt062495@gmail.com>
2026-03-24 11:43:46 -0400

    Time: 26 ms (99.51%), Space: 58.9 MB (88.88%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,27 +1,27 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
+        
         Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
 
         for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) break;
             if (i > 0 && nums[i] == nums[i - 1]) continue;
 
-            int a = nums[i];
-            int l = i + 1;
-            int r = nums.length - 1;
-
+            int l = i + 1, r = nums.length - 1;
             while (l < r) {
-                int sum = a + nums[l] + nums[r];
+                int sum = nums[i] + nums[l] + nums[r];
+
                 if (sum > 0) {
-                    r--;
+                    r --;
                 }
                 else if (sum < 0) {
-                    l++;
+                    l ++;
                 }
                 else {
-                    res.add(Arrays.asList(a, nums[l], nums[r]));
+                    res.add(Arrays.asList(nums[i], nums[l], nums[r]));
                     l++;
-
+                    r--;
                     while (l < r && nums[l] == nums[l - 1]) {
                         l++;
                     }
@@ -29,6 +29,5 @@ class Solution {
             }
         }
         return res;
-
     }
 }
