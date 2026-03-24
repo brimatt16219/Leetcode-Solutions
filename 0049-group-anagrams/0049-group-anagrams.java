426b74463dfb329306b4b69afa54bd47f584fff6
Brian Chang <brimatt062495@gmail.com>
2026-03-24 00:50:12 -0400

    Time: 19 ms (19.07%), Space: 51.3 MB (5.53%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,29 +1,16 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        
-        Map<String, List<String>> buckets = new HashMap<>();
+        Map<String, List<String>> res = new HashMap<>();
 
         for (String s : strs) {
-
             int[] count = new int[26];
-
             for (char c : s.toCharArray()) {
-                count[c - 'a'] ++;
+                count[c - 'a']++;
             }
-
-            StringBuilder keyBuilder = new StringBuilder();
-
-            for (int freq : count) {
-                keyBuilder.append('#').append(freq);
-            }
-
-            String key = keyBuilder.toString();
-            
-            buckets
-                .computeIfAbsent(key, k -> new ArrayList<>())
-                .add(s);
+            String key = Arrays.toString(count);
+            res.putIfAbsent(key, new ArrayList<>());
+            res.get(key).add(s);
         }
-
-        return new ArrayList<>(buckets.values());
+        return new ArrayList<>(res.values());
     }
 }
