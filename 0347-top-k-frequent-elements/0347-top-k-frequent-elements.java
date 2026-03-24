b678ad401679f184c5e501a76dd03b6908244112
Brian Chang <brimatt062495@gmail.com>
2026-03-24 00:58:11 -0400

    Time: 16 ms (39.97%), Space: 47.5 MB (76.14%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,30 +1,22 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-
         Map<Integer, Integer> count = new HashMap<>();
-
-        for (int n : nums) {
-            count.put(n, count.getOrDefault(n, 0) + 1);
+        for (int num : nums) {
+            count.put(num, count.getOrDefault(num, 0) + 1);
         }
 
-        List<Integer>[] buckets = new List[nums.length + 1];
-
-        for (int i = 0; i < buckets.length; i++) {
-            buckets[i] = new ArrayList<>();
-        }
-        for (Map.Entry<Integer, Integer> e : count.entrySet()) {
-            int num = e.getKey();
-            int freq = e.getValue();
-            buckets[freq].add(num);
-        }
-        
-        int[] res = new int[k];
-        int idx = 0;
-        for (int freq = buckets.length - 1; freq >= 1 && idx < k; freq--) {
-            for (int num : buckets[freq]) {
-                res[idx++] = num;
-                if (idx == k) return res;
+        PriorityQueue<int[]> heap = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
+            heap.offer(new int[] {entry.getValue(), entry.getKey()});
+            if (heap.size() > k) {
+                heap.poll();
             }
+        }
+        System.out.println(heap);
+
+        int[] res = new int[k];
+        for (int i = 0; i < k; i++) {
+            res[i] = heap.poll()[1];
         }
         return res;
     }
