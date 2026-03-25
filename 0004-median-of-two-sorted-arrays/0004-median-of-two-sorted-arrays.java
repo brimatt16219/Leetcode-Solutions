cbaad239a029dfcadd95f925f06dd14ec6b968d0
Brian Chang <brimatt062495@gmail.com>
2026-03-25 08:55:23 -0400

    Time: 1 ms (99.9%), Space: 48.8 MB (64.8%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,26 +1,41 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        ArrayList<Integer> list = new ArrayList();
-        
-        for (int i = 0; i < nums1.length; i++)
-        {
-            list.add(nums1[i]);
+        int[] A = nums1;
+        int[] B = nums2;
+        int total = A.length + B.length;
+        int half = (total + 1) / 2;
+
+        if (B.length < A.length) {
+            int[] temp = A;
+            A = B;
+            B = temp;
         }
-        for (int i = 0; i < nums2.length; i++)
-        {
-            list.add(nums2[i]);
+
+        int l = 0;
+        int r = A.length;
+        while (l <= r) {
+            int i = (l + r) / 2;
+            int j = half - i;
+
+            int Aleft = i > 0 ? A[i - 1] : Integer.MIN_VALUE;
+            int Aright = i < A.length ? A[i] : Integer.MAX_VALUE;
+            int Bleft = j > 0 ? B[j - 1] : Integer.MIN_VALUE;
+            int Bright = j < B.length ? B[j] : Integer.MAX_VALUE;
+
+            // Partition is correct
+            if (Aleft <= Bright && Bleft <= Aright) {
+                if (total % 2 != 0) {
+                    return Math.max(Aleft, Bleft);
+                }
+                return (Math.max(Aleft, Bleft) + Math.min(Aright, Bright)) / 2.0;
+            }
+            else if (Aleft > Bright) {
+                r = i - 1;
+            }
+            else {
+                l = i + 1;
+            }
         }
-        
-        Collections.sort(list);
-        int total = nums1.length + nums2.length;
-        boolean isOdd = total % 2 == 1;
-        if (isOdd) 
-        {
-            return (list.get(total / 2));
-        }
-        else
-        {
-            return (list.get(total / 2) + list.get(total / 2 - 1)) / 2.0;
-        }
+        return -1;
     }
 }
