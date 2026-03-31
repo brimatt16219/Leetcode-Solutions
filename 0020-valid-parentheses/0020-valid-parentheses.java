2d452dbc0941969635cd162b71a9fac7a164f72b
Brian Chang <brimatt062495@gmail.com>
2026-03-31 09:40:49 -0400

    Time: 4 ms (41.33%), Space: 43.6 MB (13.06%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -2,18 +2,18 @@ class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
         Map<Character, Character> closeToOpen = new HashMap<>();
+
         closeToOpen.put(')', '(');
         closeToOpen.put(']', '[');
         closeToOpen.put('}', '{');
-
-        for (char c : s.toCharArray()) {
-            if (closeToOpen.containsKey(c)) {
+        for (char c : s.toCharArray()) { 
+            if (closeToOpen.containsKey(c)) { // closing bracket
                 if (!stack.isEmpty() && stack.peek() == closeToOpen.get(c)) {
                     stack.pop();
                 }
                 else return false;
             }
-            else {
+            else { // opening bracket
                 stack.push(c);
             }
         }
