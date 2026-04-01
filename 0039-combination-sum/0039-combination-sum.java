2cde8ba81bec11907b3e1c1c2a8ab87028d92300
Brian Chang <brimatt062495@gmail.com>
2026-04-01 16:45:22 -0400

    Time: 25 ms (8.57%), Space: 47.4 MB (5.38%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,25 +1,24 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        
-        List<List<Integer>> list = new ArrayList<>();
-        Arrays.sort(candidates);
-        backtrack(list, new ArrayList<>(), candidates, target, 0);
-        return list;
-
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> subset = new ArrayList<>();
+        dfs(candidates, 0, res, subset, target);
+        return res;
     }
 
-    void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] nums, int remain, int start) {
-        System.out.println(tempList);
-        System.out.println("Start: " + start + " remain " + remain);
-        if (remain < 0) return;
-        else if (remain == 0) list.add(new ArrayList<>(tempList));
-        else {
-            for (int i = start; i < nums.length; i++) {
-                tempList.add(nums[i]);
-                
-                backtrack(list, tempList, nums, remain - nums[i], i);
-                tempList.remove(tempList.size() - 1);
-            }
+    static void dfs(int[] candidates, int k, List<List<Integer>> res, List<Integer> subset, int target) {
+        int sum = subset.stream().mapToInt(Integer::intValue).sum();
+        if (sum == target) {
+            res.add(new ArrayList<>(subset));
+            return;
         }
+        if (k >= candidates.length || sum > target) {
+            return;
+        }
+
+        subset.add(candidates[k]);
+        dfs(candidates, k, res, subset, target);
+        subset.remove(subset.size() - 1);
+        dfs(candidates, k + 1, res, subset, target);
     }
 }
