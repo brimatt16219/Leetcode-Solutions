b142740f5754f73471ef7f6ea88198bf4b0acad7
Brian Chang <brimatt062495@gmail.com>
2026-04-15 20:09:53 -0400

    Time: 46 ms (84.38%), Space: 88.4 MB (92.86%) - LeetHub

--- 3649-number-of-perfect-pairs/3649-number-of-perfect-pairs.java
+++ 3649-number-of-perfect-pairs/3649-number-of-perfect-pairs.java
@@ -2,22 +2,29 @@ class Solution {
     public long perfectPairs(int[] nums) {
         int n = nums.length;
 
+        // Take absolute values and sort
         long[] arr = new long[n];
         for (int i = 0; i < n; i++) {
-            arr[i] = Math.abs((long)nums[i]);
+            arr[i] = Math.abs((long) nums[i]);
         }
         Arrays.sort(arr);
 
         long count = 0;
         int j = 0;
+
         for (int i = 0; i < n; i++) {
+            // Make sure j is always ahead of i
             if (j <= i) j = i + 1;
 
+            // Advance j while arr[j] fits in the window
             while (j < n && arr[j] <= 2 * arr[i]) {
                 j++;
             }
+
+            // All elements from i+1 to j-1 are valid partners
             count += (j - i - 1);
         }
+
         return count;
     }
 }
