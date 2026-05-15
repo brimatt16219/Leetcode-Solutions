dca94d9296a68674056f61debc40b6253f9052c4
Brian Chang <brimatt062495@gmail.com>
2026-05-15 10:11:18 -0400

    Time: 4 ms (98.66%), Space: 47.4 MB (66.17%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.java
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.java
@@ -4,7 +4,7 @@ public class Codec {
     public String encode(List<String> strs) {
         StringBuilder res = new StringBuilder();
         for (String s : strs) {
-            res.append(s.length()).append('#').append(s);
+            res.append(s.length()).append("#").append(s);
         }
         return res.toString();
     }
