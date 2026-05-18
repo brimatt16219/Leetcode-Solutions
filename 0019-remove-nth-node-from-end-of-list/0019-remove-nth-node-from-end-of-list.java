4f81d2db57bf19e83cfca0041a89306d0e2c71f7
Brian Chang <brimatt062495@gmail.com>
2026-05-18 14:36:41 -0400

    Time: 0 ms (100%), Space: 43.4 MB (57%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -13,17 +13,14 @@ class Solution {
         ListNode dummy = new ListNode(0, head);
         ListNode left = dummy;
         ListNode right = head;
-
-        while (n > 0 && right != null) {
+        while (n > 0) {
             right = right.next;
             n--;
         }
-
         while (right != null) {
-            right = right.next;
             left = left.next;
+            right = right.next;
         }
-
         left.next = left.next.next;
         return dummy.next;
     }
