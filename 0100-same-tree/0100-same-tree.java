83af7ce1a2385f9a7ab3b8057ac6475c0ca64cc6
Brian Chang <brimatt062495@gmail.com>
2026-05-18 09:48:01 -0400

    Time: 0 ms (100%), Space: 43 MB (15.14%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -15,12 +15,11 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if (p != null && q != null) {
-            if (p.val != q.val) return false;
-            else return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
-        }
-        else {
-            return p == null && q == null;
-        }
+        if (p == null && q == null) return true;
+
+        if (p != null && q != null && p.val == q.val) {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        } 
+        return false;
     }
 }
