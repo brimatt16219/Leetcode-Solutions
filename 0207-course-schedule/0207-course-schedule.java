88119ee21053aaf4017549032e2d8ed11cb79e3a
Brian Chang <brimatt062495@gmail.com>
2026-05-19 10:41:19 -0400

    Time: 7 ms (59.24%), Space: 47 MB (62.25%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,33 +1,40 @@
 class Solution {
+
+    private Map<Integer, List<Integer>> preMap = new HashMap<>();
+    private Set<Integer> visited = new HashSet<>();
+
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        int[] indegree = new int[numCourses];
-        List<List<Integer>> adj = new ArrayList<>();
         for (int i = 0; i < numCourses; i++) {
-            adj.add(new ArrayList<>());
+            preMap.put(i, new ArrayList<>());
         }
-        for (int[] pre : prerequisites) {
-            indegree[pre[1]]++;
-            adj.get(pre[0]).add(pre[1]);
+        for (int[] prereq : prerequisites) {
+            preMap.get(prereq[0]).add(prereq[1]);
         }
 
-        Queue<Integer> q = new LinkedList<>();
-        for (int i = 0; i < numCourses; i++) {
-            if (indegree[i] == 0) {
-                q.add(i);
+        for (int c = 0; c < numCourses; c++) {
+            if (!dfs(c)) {
+                return false;
             }
+        }
+        return true;
+    }
+
+    public boolean dfs(int crs) {
+        if (visited.contains(crs)) {
+            return false;
+        }
+        if (preMap.get(crs).isEmpty()) {
+            return true;
         }
 
-        int finish = 0;
-        while (!q.isEmpty()) {
-            int node = q.poll();
-            finish++;
-            for (int nei : adj.get(node)) {
-                indegree[nei]--;
-                if (indegree[nei] == 0) {
-                    q.add(nei);
-                }
+        visited.add(crs);
+        for (int pre : preMap.get(crs)) {
+            if (!dfs(pre)) {
+                return false;
             }
         }
-        return finish == numCourses;
+        visited.remove(crs);
+        preMap.put(crs, new ArrayList<>());
+        return true;
     }
 }
