6cfc490010c11c48a06f71e73e71d91c1266c903
Brian Chang <brimatt062495@gmail.com>
2026-05-20 16:06:59 -0400

    Time: 2 ms (37.58%), Space: 44.7 MB (99.75%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,34 +1,26 @@
 class Solution {
-
-    boolean[] used;
-    List<List<Integer>> list;
+    List<List<Integer>> res;
 
     public List<List<Integer>> permute(int[] nums) {
-        used = new boolean[nums.length];
-        list = new ArrayList<>();
-        backtrack(list, new ArrayList(), nums);
-
-        return list;
+        res = new ArrayList<>();
+        boolean[] used = new boolean[nums.length];
+        backtrack(new ArrayList<>(), nums, used);
+        return res;
     }
 
-    void backtrack(List<List<Integer>> list, List<Integer> path, int[] nums) {
-        if (path.size() == nums.length) {
-            list.add(new ArrayList<>(path));
+    public void backtrack(List<Integer> perm, int[] nums, boolean[] used) {
+        if (perm.size() == nums.length) {
+            res.add(new ArrayList<>(perm));
             return;
         }
-
         for (int i = 0; i < nums.length; i++) {
-
-            if (used[i]) continue;
-            used[i] = true;
-            path.add(nums[i]);
-
-            backtrack(list, path, nums);
-
-            path.remove(path.size() - 1);
-            used[i] = false;
+            if (!used[i]) {
+                perm.add(nums[i]);
+                used[i] = true;
+                backtrack(perm, nums, used);
+                used[i] = false;
+                perm.remove(perm.size() - 1);
+            }
         }
-
-        return;
     }
 }
