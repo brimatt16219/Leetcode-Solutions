a2df52a2ded423fe9f66258d8ce59f713f3e481e
Brian Chang <brimatt062495@gmail.com>
2026-05-20 15:45:50 -0400

    Time: 2 ms (11.77%), Space: 43.7 MB (58.34%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int myAtoi(String s) {
+        s = s.stripLeading();
+
+        if (s.length() == 0) {
+            return 0;
+        }
+        int i = 0;
+        int sign = 1;
+        if (s.charAt(i) == '+') {
+            i++;
+        }
+        else if (s.charAt(i) == '-') {
+            i++;
+            sign = -1;
+        }
+
+        long parsed = 0;
+        while (i < s.length()) {
+            char c = s.charAt(i);
+            if (!Character.isDigit(c)) break;
+            
+            parsed = parsed * 10 + (c - '0');
+            
+            if (parsed * sign >= Integer.MAX_VALUE) return Integer.MAX_VALUE;
+            if (parsed * sign <= Integer.MIN_VALUE) return Integer.MIN_VALUE;
+            
+            i++;
+        }
+        parsed *= sign;
+        if (parsed >= Integer.MAX_VALUE) {
+            return Integer.MAX_VALUE;
+        }
+        else if (parsed <= Integer.MIN_VALUE) {
+            return Integer.MIN_VALUE;
+        }
+        return (int) parsed;
+    }
+}
