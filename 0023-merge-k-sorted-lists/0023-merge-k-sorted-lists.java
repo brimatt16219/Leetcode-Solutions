0482dba95d33836588f6febd026a8621ee0b1738
Brian Chang <brimatt062495@gmail.com>
2026-05-20 12:56:46 -0400

    Time: 2 ms (89.47%), Space: 46.9 MB (28.82%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -10,38 +10,41 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        if (lists.length == 0) return null;
-        List<Integer> list = new ArrayList<Integer>();
-        System.out.println(list);
-        
-        for (int i = 0; i < lists.length; i++)
-        {
-            if (lists[i] != null)
-            {
-                ListNode temp = lists[i];
-                while(temp != null)
-                {
-                    System.out.println("Adding!");
-                    list.add(temp.val);
-                    temp = temp.next;
-                }
+        if (lists == null || lists.length == 0) return null;
+
+        while (lists.length > 1) {
+            List<ListNode> mergedLists = new ArrayList<>();
+            for (int i = 0; i < lists.length; i += 2) {
+                ListNode l1 = lists[i];
+                ListNode l2 = (i + 1) < lists.length ? lists[i + 1] : null;
+                mergedLists.add(mergeList(l1, l2));
             }
+            lists = mergedLists.toArray(new ListNode[0]);
         }
-        if (list.size() == 0) return null;
-        Collections.sort(list);
-        System.out.println(list);
-        
-        ListNode head = new ListNode();
-        ListNode temp = head;
-        for (int i = 0; i < list.size(); i++)
-        {
-            head.val = list.get(i);
-            if (i < list.size() - 1){
-                head.next = new ListNode();
-                head = head.next;
+        return lists[0];
+    }
+
+    public ListNode mergeList(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode();
+        ListNode tail = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                tail.next = l1;
+                l1 = l1.next;
             }
-            
+            else {
+                tail.next = l2;
+                l2 = l2.next;
+            }
+            tail = tail.next;
         }
-        return temp;
+        if (l1 != null) {
+            tail.next = l1;
+        }
+        if (l2 != null) {
+            tail.next = l2;
+        }
+        return dummy.next;
     }
 }
