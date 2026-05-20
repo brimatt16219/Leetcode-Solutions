759d7c41ce5ccad76d49578d96d085c057e8e77c
Brian Chang <brimatt062495@gmail.com>
2026-05-20 19:39:47 -0400

    Time: 0 ms (100%), Space: 44.8 MB (28.19%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,15 +1,21 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        
-        int left = 0;
-        int right = nums.length - 1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (nums[mid] >= target) right = mid - 1;
-            else left = mid + 1;
+        int res = nums.length;
+        int l = 0;
+        int r = nums.length - 1;
+        while (l <= r) {
+            int m = l + ((r - l) / 2);
+            if (nums[m] == target) {
+                return m;
+            }
+            if (nums[m] > target) {
+                res = m;
+                r = m - 1;
+            }
+            else {
+                l = m + 1;
+            }
         }
-        return left;
+        return res;
     }
 }
