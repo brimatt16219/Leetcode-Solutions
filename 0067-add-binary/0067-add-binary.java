d937ba102b4fbc6db4bff867453e33b3b4b6a232
Brian Chang <brimatt062495@gmail.com>
2026-05-20 23:01:37 -0400

    Time: 1 ms (99.95%), Space: 43.4 MB (68.43%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,10 +1,10 @@
 class Solution {
     public String addBinary(String a, String b) {
         StringBuilder sb = new StringBuilder();
-
-        int i = a.length() - 1, j = b.length() - 1;
         int carry = 0;
 
+        int i = a.length() - 1;
+        int j = b.length() - 1;
         while (i >= 0 || j >= 0 || carry != 0) {
             int sum = carry;
             if (i >= 0) {
@@ -15,9 +15,8 @@ class Solution {
             }
 
             sb.append((char)('0' + (sum & 1)));
-            carry = sum >> 1;
+            carry = sum / 2;
         }
-
         return sb.reverse().toString();
     }
 }
